e231c7830633e301e27646c5a28a6f687fbdc53d
pelaajahacks <ronttipontti.sundstrom2@gmail.com>
2022-09-05 23:32:20 +0300

    improvement

--- JS/Content.js
+++ JS/Content.js
@@ -1,10 +1,10 @@
-var Body = document.getElementById ('Body');
-var Hdr = document.getElementById ('Nav');
-var AbtMe = document.getElementById ('AbtMe');
+var Body = document.getElementById('Body');
+var Hdr = document.getElementById('Nav');
+var AbtMe = document.getElementById('AbtMe');
 
-window.onscroll = function () {ScrollFn ()};
+window.onscroll = function() { ScrollFn() };
 
-function ScrollFn () {
+function ScrollFn() {
     if (Body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         Hdr.style.top = "-50px";
     } else {
@@ -12,39 +12,38 @@ function ScrollFn () {
     }
 }
 
-var pocoguy = document.getElementById ('credits');
-var excel = document.getElementById ('mycredits')
+var pocoguy = document.getElementById('credits');
+var excel = document.getElementById('mycredits')
 
-pocoguy.onclick = function () {
-    location.assign ('https://tiktok.com/@pocoguy.exe');
+pocoguy.onclick = function() {
+    location.assign('https://tiktok.com/@pocoguy.exe');
 }
 
-excel.onclick = function () {
-    location.assign ('https://tiktok.com/@byexcel')
+excel.onclick = function() {
+    location.assign('https://tiktok.com/@byexcel')
 }
 
 
-window.onload = function () {
+window.onload = function() {
     Hdr.style.top = "0px";
     pocoguy.style.cursor = "pointer";
     excel.style.cursor = "pointer";
 }
 
-AbtMe.onclick = function () {
-    window.location.assign ('https://linktr.ee/ItzzExcel');
-}
 
 
-var installbtn = document.getElementById ('installbtn');
+var installbtn = document.getElementById('installbtn');
 
-installbtn.onclick = function (params) {
-    location.assign ('phonbricked.html');
+installbtn.onclick = function(params) {
+    location.assign('pphonbrciked');
 }
 
 // This page is the worst front-end attempt of my whole life haha
 // Developed by https://tiktok.com/@byexcel
+// forked by https://tiktok.com/@www.segso.net
+// Ok https://github.com/pelaajahacks/adrod13
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    let title = document.getElementById ('title');
-    title.innerHTML = "rip bozo 💀💀💀";
-   }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    let title = document.getElementById('title');
+    title.innerHTML = "No cap no virus 1000% working 💀💀💀";
+}
